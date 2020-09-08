f7adf4fc5dc9c0c4bad6c6f9d5797b11d793e116
liuanhai <liuanhaiMJ@163.com>
2020-09-08 23:20:37 +0800

    dialog example with bug unsolved!

---
+++ src/optionDialog/OptionDialogFrameTest.java
@@ -0,0 +1,11 @@
+package optionDialog;
+import javax.swing.*;
+
+public class OptionDialogFrameTest {
+    public static void main(String[] args){
+        JFrame frame = new OptionDialogFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setTitle("OptionDialogFrameTest");
+    }
+}
